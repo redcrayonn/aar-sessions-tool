49fb9a974a60b5c0b5bedbc4fd0123297f684211
Olaf de Mol <olafdemol@Olafs-MacBook-Pro.local>
2019-06-18 20:23:02 +0200

    working form

--- src/main.js
+++ src/main.js
@@ -4,16 +4,25 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-axios.defaults.headers.common['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjU3NzE0OTFlOWI4OWI1MTMiLCJpYXQiOjE1NjAwNzczODcsIm5iZiI6MTU2MDA3NzM4NywiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjo4MDQ3NyJ9.M-aJpkUKYda-7WCh6SPzlFy1mPYnvvcuGC5QhIszZQY'; // for all requests
-
+import Datetime from 'vue-datetime'
+// You need a specific loader for CSS files
+import 'vue-datetime/dist/vue-datetime.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Datetime);
 
 new Vue({
   router,
   store,
+  components: {
+    datetime: Datetime
+  },
   render: h => h(App)
 }).$mount('#app')
+
+
+ 
+
 
 // axios.defaults.headers.post['header1'] = 'value' // for POST requests
--- src/views/Dummy.vue
+++ src/views/Dummy.vue
@@ -1,6 +1,24 @@
 <template>
 <div>
   <h1>Template</h1>
+  <v-text-field
+            label="start"
+            v-model="startTime"
+          ></v-text-field>
+  <datetime type="datetime" v-model="startTime" ></datetime>
+    <v-text-field
+            label="start"
+            v-model="stopTime"
+          ></v-text-field>
+  <datetime type="datetime" v-model="stopTime" ></datetime>
+  <v-btn
+      :loading="loading"
+      :disabled="loading"
+      color="secondary"
+      @click="(loader = 'loading'), (sendRequest())"
+    >
+      Get sessions!
+    </v-btn>
     <ul>
       <li v-for="session in this.nonNullItems" v-bind:key="session.id" >
         <span>{{session.id}}</span>
@@ -17,24 +35,27 @@ export default {
   name: 'home',
   data () {
       return {
-          data: null
+          data: null,
+          startTime: null,
+          stopTime: null,
+          loading: null
       }
   },
   computed: {
     nonNullItems: function() {
       if(this.data && this.data.data && this.data.data.data)
       {
-        console.log(this.data);
-        console.log(this.data[0]);
         return this.data.data.data;
       }
     }
   },
-  mounted() {
-    axios.defaults.headers.common['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjU3NzE0OTFlOWI4OWI1MTMiLCJpYXQiOjE1NjAwNzczODcsIm5iZiI6MTU2MDA3NzM4NywiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjo4MDQ3NyJ9.M-aJpkUKYda-7WCh6SPzlFy1mPYnvvcuGC5QhIszZQY'; // for all requests
-    axios
-      .get('https://api.battlemetrics.com/servers/1611557/relationships/sessions?at=2019-06-09T20:30:00Z')
-      .then(response => (this.data = response))
+  methods: {
+    sendRequest: function(){
+      axios.defaults.headers.common['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjU3NzE0OTFlOWI4OWI1MTMiLCJpYXQiOjE1NjAwNzczODcsIm5iZiI6MTU2MDA3NzM4NywiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjo4MDQ3NyJ9.M-aJpkUKYda-7WCh6SPzlFy1mPYnvvcuGC5QhIszZQY'; // for all requests
+      axios
+        .get(`https://api.battlemetrics.com/servers/1611557/relationships/sessions?start=${this.startTime}&stop=${this.stopTime}`)
+        .then(response => (this.data = response))
+    }
   }
 }
 </script>
